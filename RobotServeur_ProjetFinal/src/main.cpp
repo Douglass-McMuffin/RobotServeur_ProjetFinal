58324e859ebe562ea88d80c1a2c96978ead9c880
Douglas-Mcmuffin <gacote18@gmail.com>
2023-11-08 14:40:59 -0500

    Début

---
+++ RobotServeur_ProjetFinal/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <main.h>
+
+// Il faut un code pour le déplacement avec le suiveur de ligne
+
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
---
+++ RobotServeur_ProjetFinal/src/suiveurDeLigne.c
@@ -0,0 +1,21 @@
+// La fonction devra modifier la vitesse des roues selon les capteurs
+
+//À mettre dans le fichier .h (en bas)
+float vGauche, vDroite;
+float *p_vGauche, *p_vDroite;
+p_vGauche = &vGauche;
+p_vDroite = &vDroite;
+int eGauche, eDroite;
+
+// Inscrit les valeurs des capteurs dans les bools.
+// La fonction prend 3 pointeurs pour les 3 capteurs.
+// La fonction ne retourne rien. Les variables deviennent soit 0 pour quand il voit du noir et 1 pour quand il voit du blanc
+void LireLumiere (bool *p_luxGauche, bool *p_luxCentre, bool *p_luxDroite)
+{
+
+}
+
+void ControleMoteurLigne (float *p_vGauche, float *p_v_droite,)
+{
+    
+}
