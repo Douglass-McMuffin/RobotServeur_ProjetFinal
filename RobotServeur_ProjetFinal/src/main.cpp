95d0a022eab4c20258cc0bfa3c05975844a9fea0
MasterJoe23 <marj1522@usherbrooke.ca>
2023-11-11 20:18:09 -0500

    Squashed commit of the following:
    commit 879d38a6d91d1293216d8d8284bfe04cd5d64ced
    Author: Douglas-Mcmuffin <gacote18@gmail.com>
    Date:   Sat Nov 11 20:09:16 2023 -0500
    
        fin de soirée
    
    commit 9297b54ce795492b418d44d1b34cc4324e41258f
    Author: Douglas-Mcmuffin <gacote18@gmail.com>
    Date:   Sat Nov 11 19:00:50 2023 -0500
    
        bonjour

--- RobotServeur_ProjetFinal/src/main.cpp
+++ RobotServeur_ProjetFinal/src/main.cpp
@@ -2,11 +2,26 @@
 #include <LibRobus.h>
 
 // Il faut un code pour le déplacement avec le suiveur de ligne
+/*
 
+Dans Alignement : 
+Une fonction qui regarder les trois capteurs sont sur du noir.
+    Cette fonction
+
+
+POUR LE MOUVEMENT DU DÉPART AU CLIENT
+
+On suppose qu'on sait c'est quel client (le deuxième)
+On suppose qu'on sait qu'on est au départ
+
+Tant qu'il n'est pas au client
+
+*/
 
 void setup() {
   // put your setup code here, to run once:
   BoardInit();
+  //BONJOUURRRRR
 }
 
 void loop() {
