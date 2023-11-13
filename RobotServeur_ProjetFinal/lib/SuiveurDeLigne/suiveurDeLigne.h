d36df48b79b5dde7072dbf484dce1eb5c6ff6dd8
Douglas-Mcmuffin <gacote18@gmail.com>
2023-11-13 14:04:04 -0500

    Reprise 13 novembre

---
+++ RobotServeur_ProjetFinal/lib/SuiveurDeLigne/suiveurDeLigne.h
@@ -0,0 +1,29 @@
+#ifndef SUIVEURDELIGNE
+
+#include <libRobus.h>
+#include <arduino.h>
+
+
+#define PIN_LUMIERE_GAUCHE
+#define PIN_LUMIERE_CENTRE
+#define PIN_LUMIERE_DROITE
+
+#define NOMBRE_DE_SOMMET
+
+struct Sommet {
+    char nom;
+    struct Sommet *voisin[NOMBRE_DE_SOMMET - 1]
+    int nb_voisin;
+    struct Sommet *predecesseur;
+    bool marque = false;
+}
+
+struct File {
+    struct Sommet sommets[NOMBRE_DE_SOMMET];
+    int debut = 0;
+    int fin = 0;
+}
+
+
+
+#endif
